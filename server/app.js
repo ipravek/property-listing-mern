0187815223cc54172f3579afc828d6e4555f906a
Pk <pravin.pk4068699@gmail.com>
2023-07-28 14:07:36 +0530

    Logger added

--- server/app.js
+++ server/app.js
@@ -2,9 +2,25 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const db = require("./db/db");
+const morgan = require("morgan");
+const fs = require("fs");
+const path = require("path");
+const rfs = require("rotating-file-stream");
 
 // Db connection
 db.connect();
+
+// Logger
+const accessLogStream = rfs.createStream("access.log", {
+  interval: "1d",
+  path: path.join(__dirname, "log"),
+});
+
+app.use(
+  morgan("combined", {
+    stream: accessLogStream,
+  })
+);
 
 // Body Parser
 app.use(express.json());
